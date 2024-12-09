ee8873dce45b697ef34642d725b223bd11602c36
Aiko <aikonurfajrindiapati@gmail.com>
2024-12-09 13:50:38 +0700

    onClick state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import './App.css';
+// import './App.css';
+import { useState } from 'react';
+import logo from './assets/logo.png';
+import NavItems from './components/navItems';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('Dashboard');
+  const menu = [
+    {
+      title: 'Dashboard',
+      link: '#',
+    },
+    {
+      title: 'Transfer',
+      link: '#',
+    },
+    {
+      title: 'Topup',
+      link: '#',
+    },
+    {
+      title: 'Signout',
+      link: '#',
+    },
+  ];
+
+  // const handleClick = (activeTab) => {
+  //   setActiveTab(activeTab);
+  // };
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-4">
+          <NavItems menu={menu} activeTab={activeTab} handleClick={setActiveTab} />
+        </div>
+      </nav>
     </>
   );
 }
